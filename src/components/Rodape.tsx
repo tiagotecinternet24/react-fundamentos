07f52212a115d2cc9d4af7c3b60439e3697b6cd2
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-24 10:30:15 -0300

    Adiciona objeto com CSSProperties

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,7 +1,16 @@
+import type { CSSProperties } from "react";
+
+const estilosRodape: CSSProperties = {
+  backgroundColor: "#222",
+  color: "white",
+  textAlign: "center",
+  padding: "1rem",
+};
+
 export default function Rodape() {
   return (
     <>
-      <footer>
+      <footer style={estilosRodape}>
         <h2>Pequeno site criado com React</h2>
         <p style={{ color: "red", fontWeight: "bold" }}>
           Desenvolvido por <b>Tiago</b> &copy; 2025
