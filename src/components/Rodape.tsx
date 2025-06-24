6798f570e622aebe0c1831b76a1585ea602b8098
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-24 09:45:30 -0300

    Cria e aplica classe
    Uso do className

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,10 +1,15 @@
 export default function Rodape() {
   return (
-    <footer>
-      <h2>Pequeno site criado com React</h2>
-      <p>
-        Desenvolvido por <b>Tiago</b> &copy; 2025
-      </p>
-    </footer>
+    <>
+      <footer>
+        <h2>Pequeno site criado com React</h2>
+        <p>
+          Desenvolvido por <b>Tiago</b> &copy; 2025
+        </p>
+      </footer>
+      <hr />
+      <p className="outro-texto">Qualquer coisa...</p>
+      <hr />
+    </>
   );
 }
