c526abb3568d6a248ec346702d85d17eb61e2694
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-24 11:12:59 -0300

    Adiciona classe pra titulo e logo

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -7,8 +7,8 @@ import estilos from "./Cabecalho.module.css";
 export default function Cabecalho() {
   return (
     <header className={estilos.topoSite}>
-      <h1>
-        <img src={logo} alt="" /> Olá React! 😎
+      <h1 className={estilos.titulo}>
+        <img className={estilos.logo} src={logo} alt="" /> Olá React! 😎
       </h1>
       <hr />
       <Menu />
