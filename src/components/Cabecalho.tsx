d462d6a822c919d39e6538df6539a9af1c160c9f
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-24 08:58:21 -0300

    Cria componente Menu

--- src/components/Cabecalho.tsx
+++ src/components/Cabecalho.tsx
@@ -1,6 +1,5 @@
-/* Tudo o que for usado dentro de componentes (imagens, fontes etc), 
-deve ser importado. Desta forma, no momento do build, haverá otimizações para este conteúdo. */
 import logo from "../assets/logo-react.png";
+import Menu from "./Menu";
 
 export default function Cabecalho() {
   return (
@@ -9,11 +8,7 @@ export default function Cabecalho() {
         <img src={logo} alt="" /> Olá React! 😎
       </h1>
       <hr />
-      <nav>
-        <a href="">Home</a>
-        <a href="">Sobre</a>
-        <a href="">Contato</a>
-      </nav>
+      <Menu />
     </header>
   );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,9 @@
+export default function Menu() {
+  return (
+    <nav>
+      <a href="">Home</a>
+      <a href="">Sobre</a>
+      <a href="">Contato</a>
+    </nav>
+  );
+}
