477447e99e4b2a3bb4df0f84cb9f356fdeed51aa
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-24 09:14:20 -0300

    Cria e reutiliza componente Artigo

---
+++ src/components/Artigo.tsx
@@ -0,0 +1,13 @@
+export default function Artigo() {
+  return (
+    <article>
+      <h3>Artigo 1 de exemplo</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, et
+        voluptatem dignissimos dolorum necessitatibus sint quod consectetur
+        aliquid! Officiis adipisci totam natus dicta laborum quisquam fugit
+        corrupti nesciunt, quasi sed?
+      </p>
+    </article>
+  );
+}
--- src/components/Conteudo.tsx
+++ src/components/Conteudo.tsx
@@ -1,3 +1,5 @@
+import Artigo from "./Artigo";
+
 export default function Conteudo() {
   return (
     <main>
@@ -5,35 +7,11 @@ export default function Conteudo() {
         <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
-        <article>
-          <h3>Artigo 1 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
-            quod consectetur aliquid! Officiis adipisci totam natus dicta
-            laborum quisquam fugit corrupti nesciunt, quasi sed?
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3>Artigo 2 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
-            quod consectetur aliquid! Officiis adipisci totam natus dicta
-            laborum quisquam fugit corrupti nesciunt, quasi sed?
-          </p>
-        </article>
+        <Artigo />
 
-        <article>
-          <h3>Artigo 3 de exemplo</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            Asperiores, et voluptatem dignissimos dolorum necessitatibus sint
-            quod consectetur aliquid! Officiis adipisci totam natus dicta
-            laborum quisquam fugit corrupti nesciunt, quasi sed?
-          </p>
-        </article>
+        <Artigo />
       </section>
     </main>
   );
