e4fbd8235d53d488f513772e1ed5663cd7b770d6
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-25 10:56:18 -0300

    Reorganiza componente e styled

--- src/components/DicaDoDia/DicaDoDia.tsx
+++ src/components/DicaDoDia/DicaDoDia.tsx
@@ -1,43 +1,4 @@
-import styled from "styled-components";
-
-// Simulando uma mudança de tema
-let tema: boolean = false;
-
-const StyledDicaDoDia = styled.section`
-  // Avaliando se o tema simulado é true (aplica verde) ou false (aplica azul)
-  background-color: ${tema ? " #bce8bc" : "lightblue"};
-  border-left: 5px solid darkgreen;
-  padding: 1rem;
-  margin-top: 1rem;
-  border-radius: 4px;
-
-  h3 {
-    margin-top: 0;
-    color: darkgreen;
-  }
-
-  p {
-    color: green;
-    font-size: 1.1rem;
-  }
-
-  .site-oficial {
-    background-color: white;
-    text-align: center;
-    padding: 0.4rem;
-
-    a {
-      text-decoration: none;
-      color: darkgreen;
-      font-weight: bold;
-
-      &:hover,
-      &:focus {
-        text-decoration: underline;
-      }
-    }
-  }
-`;
+import { StyledDicaDoDia } from "./styles";
 
 export default function DicaDoDia() {
   return (
