eb6d6614a1c0643773138fa1dc3b4bd068ea0895
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-25 11:48:10 -0300

    Aplica classes tailwind

--- src/components/Conteudo/AvisoImportante/AvisoImportante.tsx
+++ src/components/Conteudo/AvisoImportante/AvisoImportante.tsx
@@ -1,3 +1,14 @@
 export default function AvisoImportante() {
-  return <div>AvisoImportante</div>;
+  return (
+    <section className="bg-yellow-200 border-l-4 border-yellow-600 p-4 mt-4 rounded">
+      <h3 className="font-bold text-2xl text-orange-800">Aviso importante:</h3>
+      <p>Alguns alunos e alunas estão perigando reprovação nesta UC.</p>
+
+      <div className="flex justify-between my-3 bg-white">
+        <div className="bg-green-200">UM</div>
+        <div className="bg-blue-300">DOIS</div>
+        <div className="bg-red-300">TRÊS</div>
+      </div>
+    </section>
+  );
 }
--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -9,7 +9,9 @@ export default function Conteudo() {
       <section
         className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
       >
-        <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
+        <h2 className="font-bold text-lg">
+          Seja bem-vindo(a) aos Fundamentos de React!
+        </h2>
         <p>Este é um exemplo de aplicação React.</p>
 
         <Artigo />
