64abf5e0f35643b57a9cf548059679df52d87e89
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-25 09:48:26 -0300

    Update DicaDoDia.tsx

--- src/components/DicaDoDia/DicaDoDia.tsx
+++ src/components/DicaDoDia/DicaDoDia.tsx
@@ -1,3 +1,18 @@
 export default function DicaDoDia() {
-  return <div>DicaDoDia</div>;
+  return (
+    <section>
+      <h3>Dica do Dia</h3>
+      <p>
+        Você pode combinar diferentes formas de estilização em um projeto React.
+        Neste exemplo, estamos usando <b>styled-components</b>, uma famosa
+        biblioteca para CSS In JS.
+      </p>
+      <p>
+        Site oficial:{" "}
+        <a href="https://styled-components.com/" target="_blank">
+          Styled Components
+        </a>
+      </p>
+    </section>
+  );
 }
