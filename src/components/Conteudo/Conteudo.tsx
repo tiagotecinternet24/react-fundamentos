82c6d39318bfe9565a5d28d59ac478a74d00b783
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-25 09:14:08 -0300

    Formata Conteudo e combina classes

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -1,9 +1,11 @@
 import Artigo from "../Artigo/Artigo";
+import estilos from "./Conteudo.module.css";
 
 export default function Conteudo() {
   return (
-    <main>
-      <section>
+    <main className={estilos.conteudo}>
+      {/* Para combinar classes, use template string */}
+      <section className={`${estilos.arredondada} ${estilos.sombra}`}>
         <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
