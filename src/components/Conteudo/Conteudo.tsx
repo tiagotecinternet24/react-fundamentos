ee8186fda8836623a9a11f818ca8078fecdfa03e
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-25 10:28:49 -0300

    Ajusta regra do módulo de Conteudo

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -5,7 +5,9 @@ import estilos from "./Conteudo.module.css";
 export default function Conteudo() {
   return (
     <main className={estilos.conteudo}>
-      <section className={`${estilos.arredondada} ${estilos.sombra}`}>
+      <section
+        className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
+      >
         <h2>Seja bem-vindo(a) aos Fundamentos de React!</h2>
         <p>Este é um exemplo de aplicação React.</p>
 
