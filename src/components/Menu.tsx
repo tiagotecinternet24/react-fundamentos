9154a2bd9fceaa6ac4904697de989527bc351eab
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-25 08:42:55 -0300

    Cria módulo e regra para menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,8 @@
+import estilos from "./Menu.module.css";
+
 export default function Menu() {
   return (
-    <nav>
+    <nav className={estilos.menu}>
       <a href="">Home</a>
       <a href="">Sobre</a>
       <a href="">Contato</a>
