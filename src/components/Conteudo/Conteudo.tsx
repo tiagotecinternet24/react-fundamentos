d84bf630cbddd2747ae04ff22c5481f4987a3f63
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-26 11:26:55 -0300

    Aplica map para carregar Artigo com dados do curso

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -19,6 +19,10 @@ export default function Conteudo() {
         {/* Faça a lógica necessária para apresentar o componente
         Artigo e passar para ele os dados de cada curso proveniente
         de cursos. */}
+
+        {cursos.map((curso) => {
+          return <Artigo dados={curso} key={curso.id} />;
+        })}
       </section>
 
       <DicaDoDia />
