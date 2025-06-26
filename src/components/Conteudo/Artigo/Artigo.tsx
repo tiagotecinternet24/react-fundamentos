d16b25bee4dd6c292feea12122ddd8799c1a73da
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-26 09:30:19 -0300

    Aplica children

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,18 +1,19 @@
+import type { ReactNode } from "react";
 import estilos from "./Artigo.module.css";
 
 type ArtigoProps = {
   titulo: string;
-  texto: string;
   numero: number;
+  children: ReactNode;
 };
 
-export default function Artigo({ titulo, texto, numero }: ArtigoProps) {
+export default function Artigo({ titulo, numero, children }: ArtigoProps) {
   return (
     <article className={estilos.artigo}>
       <h3>
         Artigo {numero}: {titulo}
       </h3>
-      <p>{texto}</p>
+      {children}
     </article>
   );
 }
