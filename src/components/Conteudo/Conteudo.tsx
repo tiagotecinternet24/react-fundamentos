82d3a2302c163f531f2cec220eead008b3682cae
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-26 10:56:13 -0300

    Update Conteudo.tsx

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -3,6 +3,7 @@ import AvisoImportante from "./AvisoImportante/AvisoImportante";
 import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import estilos from "./Conteudo.module.css";
 import Saudacao from "./Saudacao";
+import cursos from "../../data/cursos";
 
 export default function Conteudo() {
   return (
@@ -12,34 +13,12 @@ export default function Conteudo() {
       >
         <h2 className="font-bold text-lg">Fundamentos de React!</h2>
         <Saudacao nome="Tiago" classe="bg-emerald-100" />
-        <Saudacao classe="bg-red-100" />
 
         <p>Este é um exemplo de aplicação React.</p>
 
-        <Artigo titulo="Front-End" numero={1} data={new Date()}>
-          <p>Front-End é muito bacana, a gente usa HTML com CSS e JavaScript</p>
-          <p>Mas também dá dor de cabeça quando usa CSS...</p>
-        </Artigo>
-
-        <Artigo titulo="Back-End" numero={2}>
-          <p>Importante estudar:</p>
-          <ul className="*:list-disc *:ml-7 ">
-            <li>PHP</li>
-            <li>SQL</li>
-            <li>Orientação a Objetos</li>
-          </ul>
-          <p>E muitos mais!</p>
-        </Artigo>
-
-        <Artigo titulo="Mobile" numero={3}>
-          <details>
-            <summary>Conheça as tecnologias mais usadas</summary>
-            <ol className="*:list-decimal *:ml-7">
-              <li>JavaScript/TypeScript</li>
-              <li>Libs e Frameworks</li>
-            </ol>
-          </details>
-        </Artigo>
+        {/* Faça a lógica necessária para apresentar o componente
+        Artigo e passar para ele os dados de cada curso proveniente
+        de cursos. */}
       </section>
 
       <DicaDoDia />
