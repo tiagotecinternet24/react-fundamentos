495eb7a12d52ca8fab3f6cd649a3f63881cc98a8
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-26 10:50:35 -0300

    Ajusta componente para os novos dados

--- src/components/Conteudo/Artigo/Artigo.tsx
+++ src/components/Conteudo/Artigo/Artigo.tsx
@@ -1,34 +1,22 @@
-import type { ReactNode } from "react";
 import estilos from "./Artigo.module.css";
+import type { Curso } from "../../../types/Curso";
 
 type ArtigoProps = {
-  titulo: string;
-  numero: number;
-  children: ReactNode;
-  data?: Date;
+  dados: Curso;
 };
 
-export default function Artigo({
-  titulo,
-  numero,
-  children,
-  data,
-}: ArtigoProps) {
+export default function Artigo({ dados }: ArtigoProps) {
+  // Podemos receber a prop dados para depois desestruturar:
+  const { titulo, preco, categoria } = dados;
   return (
     <article className={estilos.artigo}>
-      <h3>
-        Artigo {numero}: {titulo}
-      </h3>
-
-      {/* Renderização condicional: só mostra o parágrafo
-      se existir um valor na prop data. */}
-      {data && (
-        <p>
-          Data do registro: <time>{data?.toLocaleDateString()}</time>
-        </p>
-      )}
-
-      {children}
+      <h3> {titulo} </h3>
+      <p>
+        <b>Categoria:</b> {categoria}
+      </p>
+      <p>
+        <b>Preço:</b> {preco}
+      </p>
     </article>
   );
 }
