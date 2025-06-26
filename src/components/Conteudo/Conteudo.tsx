87bcb75b864914ce4ef679633b7ad6db79e86aa7
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-26 09:47:17 -0300

    Ajusta chamada do Artigo

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -16,23 +16,30 @@ export default function Conteudo() {
 
         <p>Este é um exemplo de aplicação React.</p>
 
-        <Artigo
-          titulo="Front-End"
-          texto="Um texto qualquer sobre Front"
-          numero={1}
-        />
+        <Artigo titulo="Front-End" numero={1}>
+          <p>Front-End é muito bacana, a gente usa HTML com CSS e JavaScript</p>
+          <p>Mas também dá dor de cabeça quando usa CSS...</p>
+        </Artigo>
 
-        <Artigo
-          titulo="Back-End"
-          texto="Um texto qualquer sobre Back"
-          numero={2}
-        />
+        <Artigo titulo="Back-End" numero={2}>
+          <p>Importante estudar:</p>
+          <ul className="*:list-disc *:ml-7 ">
+            <li>PHP</li>
+            <li>SQL</li>
+            <li>Orientação a Objetos</li>
+          </ul>
+          <p>E muitos mais!</p>
+        </Artigo>
 
-        <Artigo
-          titulo="Mobile"
-          texto="Um texto qualquer sobre Mobile"
-          numero={3}
-        />
+        <Artigo titulo="Mobile" numero={3}>
+          <details>
+            <summary>Conheça as tecnologias mais usadas</summary>
+            <ol className="*:list-decimal *:ml-7">
+              <li>JavaScript/TypeScript</li>
+              <li>Libs e Frameworks</li>
+            </ol>
+          </details>
+        </Artigo>
       </section>
 
       <DicaDoDia />
