c8d6ae76e41b4f289cd9c6dee4a524968e08a17d
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-27 11:18:51 -0300

    Gera array de categorias sem valores repetidos

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -3,8 +3,12 @@ import DicaDoDia from "./DicaDoDia/DicaDoDia";
 import estilos from "./Conteudo.module.css";
 import Saudacao from "./Saudacao";
 import ListaCursos from "./ListaCursos/ListaCursos";
+import cursos from "../../data/cursos";
 
 export default function Conteudo() {
+  const categorias = [...new Set(cursos.map((curso) => curso.categoria))];
+  console.log(categorias);
+
   return (
     <main className={estilos.conteudo}>
       <section
@@ -15,9 +19,6 @@ export default function Conteudo() {
 
         <p>Este é um exemplo de aplicação React.</p>
 
-        {/* Faça a lógica necessária para apresentar o componente
-        Artigo e passar para ele os dados de cada curso proveniente
-        de cursos. */}
         <ListaCursos />
       </section>
 
