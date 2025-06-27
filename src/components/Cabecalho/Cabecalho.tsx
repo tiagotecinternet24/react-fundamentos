144e5d2329f85c1c6b1d821f3e4100bedfe452ee
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-27 09:40:19 -0300

    Adiciona callback e usa setTitulo

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -13,9 +13,13 @@ export default function Cabecalho() {
   e uma função responsável por atualizar o state (setTitulo) */
   const [titulo, setTitulo] = useState("Olá React!");
 
+  const alternarTitulo = () => {
+    setTitulo("Experimentando States!");
+  };
+
   return (
     <header className={estilos.topoSite}>
-      <h1 onClick={() => alert("Olá!")} className={estilos.titulo}>
+      <h1 onClick={alternarTitulo} className={estilos.titulo}>
         <img className={estilos.logo} src={logo} alt="" /> {titulo} 😎
       </h1>
       <hr />
