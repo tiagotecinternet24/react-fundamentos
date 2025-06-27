04ca0229eef54353fcbceeec18fd97c56ba4e387
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-06-27 09:36:19 -0300

    Adiciona state para titulo

--- src/components/Cabecalho/Cabecalho.tsx
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -1,12 +1,22 @@
 import logo from "../../assets/logo-react.png";
 import Menu from "./Menu/Menu";
 import estilos from "./Cabecalho.module.css";
+import { useState } from "react";
 
 export default function Cabecalho() {
+  /* Aqui definimos a chamada de uma função especial
+  do React: useState. Esta e outras funções iniciadas com
+  o prefixo 'use' são conhecidas como React Hooks.
+  
+  No caso do useState, usando desestruturação, criamos
+  uma constante que representa o valor do state (titulo)
+  e uma função responsável por atualizar o state (setTitulo) */
+  const [titulo, setTitulo] = useState("Olá React!");
+
   return (
     <header className={estilos.topoSite}>
       <h1 onClick={() => alert("Olá!")} className={estilos.titulo}>
-        <img className={estilos.logo} src={logo} alt="" /> Olá React! 😎
+        <img className={estilos.logo} src={logo} alt="" /> {titulo} 😎
       </h1>
       <hr />
       <Menu />
