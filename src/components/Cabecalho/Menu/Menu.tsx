e9c648ad98ba688d4d657f9f4a5bad015eb609df
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-24 10:45:38 -0300

    Troca âncora por Link

--- src/components/Cabecalho/Menu/Menu.tsx
+++ src/components/Cabecalho/Menu/Menu.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import estilos from "./Menu.module.css";
 
 export default function Menu() {
   return (
     <nav className={estilos.menu}>
-      <a href="/">Home</a>
-      <a href="/sobre">Sobre</a>
-      <a href="/contato">Contato</a>
+      <Link to="/">Home</Link>
+      <Link to="/sobre">Sobre</Link>
+      <Link to="/contato">Contato</Link>
     </nav>
   );
 }
