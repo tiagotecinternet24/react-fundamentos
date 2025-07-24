9d05c1204cdd04c46eaebe51915cfab9697cf7c7
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-24 10:40:23 -0300

    Adiciona url das rotas

--- src/components/Cabecalho/Menu/Menu.tsx
+++ src/components/Cabecalho/Menu/Menu.tsx
@@ -3,9 +3,9 @@ import estilos from "./Menu.module.css";
 export default function Menu() {
   return (
     <nav className={estilos.menu}>
-      <a href="">Home</a>
-      <a href="">Sobre</a>
-      <a href="">Contato</a>
+      <a href="/">Home</a>
+      <a href="/sobre">Sobre</a>
+      <a href="/contato">Contato</a>
     </nav>
   );
 }
