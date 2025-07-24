71430d110e984274a15e35d633c2a4d029170cc8
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-24 10:56:29 -0300

    Reorganiza layout com Container

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,5 @@
+import estilos from "./Container.module.css";
+
+export default function Container() {
+  return <main className={estilos.container}></main>;
+}
--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -15,7 +15,7 @@ export default function Conteudo() {
   const categorias = [...new Set(cursos.map((curso) => curso.categoria))];
 
   return (
-    <main className={estilos.conteudo}>
+    <>
       <section
         className={`${estilos.principal} ${estilos.arredondada} ${estilos.sombra}`}
       >
@@ -55,6 +55,6 @@ export default function Conteudo() {
       <DicaDoDia />
 
       <AvisoImportante />
-    </main>
+    </>
   );
 }
