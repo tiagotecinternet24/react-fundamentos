d32f3698f2231fe6f18ee03ef33de2cc2d375373
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-24 09:54:46 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-
-// Importando a folha de estilos CSS global
 import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
