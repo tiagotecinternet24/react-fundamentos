1f77b4bbb78ea6b1afdcd020e636f40908e95b9d
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-24 11:10:29 -0300

    Configura prop children

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Rodape from "./components/Rodape/Rodape";
 import Home from "./pages/Home";
 import Sobre from "./pages/Sobre";
 import Contato from "./pages/Contato";
+import Container from "./components/Container/Container";
 
 export default function App() {
   return (
     <>
       <Cabecalho />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<Sobre />} />
-        <Route path="/contato" element={<Contato />} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </Container>
 
       <Rodape />
     </>
--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,5 +1,13 @@
+import type { ReactNode } from "react";
 import estilos from "./Container.module.css";
 
-export default function Container() {
-  return <main className={estilos.container}></main>;
+type ContainerProps = {
+  children: ReactNode;
+};
+
+/* Sempre que você quiser que o componente (Container neste caso)
+aceite conteúdo JSX dentro dele, o tipo correto para a prop children é
+ReactNode, que cobre qualquer coisa renderizável no JSX (elementos, texto etc) */
+export default function Container({ children }: ContainerProps) {
+  return <main className={estilos.container}>{children}</main>;
 }
