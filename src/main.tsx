2e04b0a86a5db1ef17241e60791b7acf0dec56be
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-24 09:59:37 -0300

    Adiciona BrowserRouter ao App

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./global.css";
+import { BrowserRouter } from "react-router-dom";
+
+/* O BrowserRouter deve envolver toda a aplicação, habilitando assim
+a navegação por rotas. */
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
