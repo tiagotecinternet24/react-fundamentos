c39095b8ad0cb0691dc57555f982b0dcd77b2349
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-25 09:31:52 -0300

    Adiciona prop key à lista de botões

--- src/components/Conteudo/Conteudo.tsx
+++ src/components/Conteudo/Conteudo.tsx
@@ -18,9 +18,10 @@ export default function Conteudo() {
 
         <p>Este é um exemplo de aplicação React.</p>
 
-        {categorias.map((categoria) => {
+        {categorias.map((categoria, index) => {
           return (
             <button
+              key={index}
               onClick={() => setCategoriaAtiva(categoria)}
               className="rounded py-1 px-2 mr-1 my-1 bg-amber-200 hover:bg-green-300"
             >
