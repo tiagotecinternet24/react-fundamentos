21958edbcc3637dddb83736c95e7279e1d882ca1
tiagotecinternet24 <tiago.tecinternet2024@gmail.com>
2025-07-25 09:23:15 -0300

    Programa useEffect básico

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,10 @@
+import { useEffect } from "react";
 import Conteudo from "../components/Conteudo/Conteudo";
 
 export default function Home() {
+  useEffect(() => {
+    console.log("Componente montado/renderizado!");
+  }, []);
+
   return <Conteudo />;
 }
